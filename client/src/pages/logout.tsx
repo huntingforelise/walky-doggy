4ee4ca77dcc300b33817c852b195a85bc3b0dbf3
Colin Howard <colinhoward89@gmail.com>
2023-03-16 09:29:33 +0000

    changed button style

--- client/src/pages/logout.tsx
+++ client/src/pages/logout.tsx
@@ -37,7 +37,7 @@ const Logout = () => {
         </button>
         <button
           color="error"
-          className="btn-login"
+          className="btn-clicked"
           onClick={handleClick}
         >
           Yes
